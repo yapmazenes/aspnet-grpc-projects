ec0a2ce8bc68ae930c13ccc1f1709ec17f6c3c43
enes.yapmaz <enes.yapmaz@memorial.com.tr>
2022-08-30 08:09:51 +0300

    Created ShoppingCartGrpc, InMemoryDb, Seed

---
+++ GrpcMicroservices/ShoppingCartGrpc/Data/ShoppingCartContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCartGrpc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartGrpc.Data
+{
+    public class ShoppingCartContext : DbContext
+    {
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+    }
+}
---
+++ GrpcMicroservices/ShoppingCartGrpc/Data/ShoppingCartContextSeed.cs
@@ -0,0 +1,49 @@
+﻿using ShoppingCartGrpc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartGrpc.Data
+{
+    public class ShoppingCartContextSeed
+    {
+        public static void Seed(ShoppingCartContext shoppingCartContext)
+        {
+            if (!shoppingCartContext.ShoppingCarts.Any())
+            {
+                var shoppingCart = new List<ShoppingCart>
+                {
+                      new ShoppingCart
+                    {
+                        UserName = "enes",
+                        Items = new List<ShoppingCartItem>
+                        {
+                            new ShoppingCartItem
+                            {
+                                Quantity = 2,
+                                Color ="Black",
+                                Price = 699,
+                                ProductId = 1,
+                                ProductName = "Mi10T"
+                            },
+                            new ShoppingCartItem
+                            {
+                                Quantity = 3,
+                                Color = "Red",
+                                Price = 899,
+                                ProductId = 2,
+                                ProductName = "P40"
+                            }
+                        }
+                    },
+
+                };
+
+                shoppingCartContext.ShoppingCarts.AddRange(shoppingCart);
+                shoppingCartContext.SaveChanges();
+            }
+        }
+    }
+}
